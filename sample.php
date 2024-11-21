58b055c0b7240092c473bbc66b422041524fb6d9
HoangAnhNguyen0710 <HoangAnhNguyen07102001@gmail.com>
2024-11-21 03:02:53 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,3 +1,6 @@
+<?php 
+    $result = ' None ' ;
+ ?> 
 <!DOCTYPE html> 
 <html  lang = " ja " > 
     <head> 
@@ -6,9 +9,9 @@
     </head> 
     <body> 
         <h1> Fortune </h1> 
-        <p> Fortune result: None </p> 
+        <p> Fortune result: <?php  print  htmlspecialchars ( $result , ENT_QUOTES , " UTF-8 " ); ?> </p> 
         <form  action = " sample.php "  method = " POST " > 
-            <button  type = " submit "  name = " act "  value = " draw " > Draw a fortune! </button> 
+            <button  type = " submit "  name = " act "  value = " draw " > Draw your fortune! </button> 
         </form> 
     </body> 
 </html>
