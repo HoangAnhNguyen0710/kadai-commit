5abd47a2acaa2015ce0921a04530ea44228dac7f
HoangAnhNguyen0710 <HoangAnhNguyen07102001@gmail.com>
2024-11-21 02:55:24 +0000

    first commit

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html  lang = " ja " > 
+    <head> 
+        <meta  charset = " UTF-8 " > 
+        <title> Fortune </title> 
+    </head> 
+    <body> 
+        <h1> Fortune </h1> 
+        <p> Fortune result: None </p> 
+        <form  action = " sample.php "  method = " POST " > 
+            <button  type = " submit "  name = " act "  value = " draw " > Draw a fortune! </button> 
+        </form> 
+    </body> 
+</html>
